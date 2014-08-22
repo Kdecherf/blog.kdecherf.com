5aca800978bfaf0891be98282e2a28ce7bc3369f
Kevin Decherf <kevin@kdecherf.com>
2014-08-22 13:19:40 +0200

    conf: add MD extension, update urls and remove links

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,26 +6,23 @@ AUTHOR = u'Kevin Decherf'
 SITENAME = u'kdecherf (git)-[blog] %'
 SITEURL = ''
 
+MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra','fenced_code']
+
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
