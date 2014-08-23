ea6ed1b5ed3689dacf4a01e7262b4b2eb65c65d4
Kevin Decherf <kevin@kdecherf.com>
2014-08-23 19:32:46 +0200

    add production disqus and GA data

--- publishconf.py
+++ publishconf.py
@@ -13,12 +13,11 @@ from pelicanconf import *
 SITEURL = 'http://blog.kdecherf.com'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = 'atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = 'kdecherf-blog'
+GOOGLE_ANALYTICS = 'UA-3230664-5'
