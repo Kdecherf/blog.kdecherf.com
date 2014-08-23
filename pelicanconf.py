f95b1006d7ee115bebd539b9065d2320b96cad56
Kevin Decherf <kevin@kdecherf.com>
2014-08-23 14:26:57 +0200

    conf: enable pagination, change archives url and remove authors page

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,15 +19,21 @@ DEFAULT_LANG = u'en'
 
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
+
+ARCHIVES_SAVE_AS = 'archives/index.html'
+ARCHIVES_URL = 'archives/'
+
+AUTHORS_SAVE_AS = ''
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
