e11288482f37a939cb94c7d88ef51c71b213dc58
Kevin Decherf <kevin@kdecherf.com>
2026-08-16 19:29:03 +0200

    chore(deps): update hugo to 0.164.0
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   # From https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=hugo
   # git rev-list -n 20 nixpkgs-unstable -- pkgs/by-name/hu/hugo/package.nix | xargs -I{} git grep -E '^\s+version\s?=\s?"[^"]+"\s*;\s*$' {} -- pkgs/by-name/hu/hugo/package.nix
   pkgs = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/9549f5d9d56a1a21d7f3f4bc80e6af5a98392c69.tar.gz";
+        url = "https://github.com/NixOS/nixpkgs/archive/023f5cbc13e335a868e07c1ce691961fca06e306.tar.gz";
       }) {};
 in
 
